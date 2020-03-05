98eff76d562e85c3d36f3ecfb07eaadf2c846758
asmtecno <alex.miranda3@etec.sp.gov.br>
2020-03-04 21:22:19 -0300

    Revisao de exemplo classificar

--- src/Exemplos.java
+++ src/Exemplos.java
@@ -2,12 +2,28 @@
 public class Exemplos {
 
 	public static void main(String[] args) {
-		int idade = 17;
+		classificar(-50);
+
+	}
+
+	public static void ehMaiorIdade(int idade) {
 		if (idade >= 18) {
 			System.out.println("Maior de idade");
 		} else {
 			System.out.println("Menor de idade");
 
+		}
+	}
+
+	public static void classificar(int num) {
+		if (num < 0) {
+			System.out.println("Negativo");
+		}
+		if (num > 0) {
+			System.out.println("Positivo");
+		} 
+		else {
+			System.out.println("zero");
 		}
 
 	}
